18b63440f8ae8a40709d92f71b1c1c5d6ca4b987
Axiom <40326513+AxiomYT@users.noreply.github.com>
2021-01-08 01:20:02 +0000

    Fixed Colouring

--- astrophotography.py
+++ astrophotography.py
@@ -12,7 +12,7 @@ pixelpitch = {
 }
 
 class calculation:
-    def __init__(self, aperture, pixel_pitch, focal_length, shutter_speed):
+    def __init__(self): # , aperture, pixel_pitch, focal_length, shutter_speed):
         self.aperture = ""       # F/Stop No.
         self.pixel_pitch = ""    # Microns
         self.focal_length = ""   # Millimetres
@@ -31,27 +31,32 @@ def configRead(config):
         config.read(r"config.cfg")
 
         calculation.pixel_pitch = config.get('Pixel Pitch','PIXEL_PITCH')
-        print("test", str(calculation.pixel_pitch))
         
         if (calculation.pixel_pitch != ""):
             pixelpitch["pixel_width"] = config.get('Pixel Pitch', 'PIXEL_WIDTH')
             pixelpitch["physical_width"] = config.get('Pixel Pitch', 'PHYSICAL_WIDTH')
+            
+            calculation.pixel_pitch = ( ( float(pixelpitch["physical_width"]) / ( float(pixelpitch["pixel_width"])) ) * 1000 )
+
+        else:
+            calculation.pixel_pitch = config.get('Pixel Pitch','PIXEL_PITCH')
 
     except:
         raise Exception('Could not open config.cfg, this could be down to a permissions error - \nOr even simply that the file does not exist, please check.\n\nhttps://github.com/AxiomYT/NPF-Rule-Calculator-Python\n\nFor further clarification.\n')
 
 def setup(config):
-    calculation.aperture = float(config.get('Specific Camera Variables', 'APERTURE'))
-    calculation.pixel_pitch = ( ( float(pixelpitch["physical_width"]) / ( float(pixelpitch["pixel_width"])) ) * 1000 )
     calculation.focal_length = (float(config.get('Specific Camera Variables', 'FOCAL_LENGTH')))
+    #calculation.pixel_pitch = ( ( float(pixelpitch["physical_width"]) / ( float(pixelpitch["pixel_width"])) ) * 1000 )
+    calculation.aperture = float(config.get('Specific Camera Variables', 'APERTURE')) 
+    
     print('\033[95m' + "\nFocal Length  -", calculation.focal_length, "Millimetres" + '\033[0m')
-    print('\033[94m' + "Pixel Pitch   -", round(calculation.pixel_pitch, 2), " Millimetres" + '\033[91m')   # Important that the output is rounded, 
-    print('\033[96m' + "Aperture      -", calculation.aperture, "\n" + '\033[91m')
+    print('\033[94m' + "Pixel Pitch   -", round(calculation.pixel_pitch, 2), " Millimetres" + '\033[0m')   # Important that the output is rounded, 
+    print('\033[96m' + "Aperture      -", calculation.aperture, "\n" + '\033[0m')
 
 def npfMethod():
     # (((35 x aperture) + (30 x pixel pitch)) ÷ focal length ) = Shutter speed in seconds.
     calculation.shutter_speed = ( (( 35 * calculation.aperture ) + ( 30 * calculation.pixel_pitch )) / (float(calculation.focal_length)))
-    print('\033[4m' + "Shutter Speed - ", (round(calculation.shutter_speed * 2.0) / 2.0), "Seconds\n" + '\033[91m')
+    print('\033[93m' + "Shutter Speed - ", (round(calculation.shutter_speed * 2.0) / 2.0), "Seconds\n" + '\033[0m')
 
 # Main
 if __name__ == "__main__":
