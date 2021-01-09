fa4cdab5956577da86d569e99371cee37c3e5826
Axiom <40326513+AxiomYT@users.noreply.github.com>
2021-01-09 01:12:37 +0000

    Brought up to par with master

--- astrophotography.py
+++ astrophotography.py
@@ -3,8 +3,6 @@ import sys
 from configparser import ConfigParser
 from pathlib import Path
 
-sys.path
-
 # To specify a custom config file location, please use this format -
 # Path("path", "to", "your", "config.cfg")
 configLocation = Path("config.cfg")
@@ -13,19 +11,20 @@ configLocation = Path("config.cfg")
 pixelpitch = {
     "physical_width" : None,     # Millimetres
     "pixel_width" : None,        # Pixels
-    "selector" : True
 }
 
 class calculation:
-    def __init__(self): # , aperture, pixel_pitch, focal_length, shutter_speed):
+    def __init__(self):
         self.aperture = ""       # F/Stop No.
         self.pixel_pitch = ""    # Microns
         self.focal_length = ""   # Millimetres
         self.shutter_speed = ""  # Seconds
 
+sys.path
+config = ConfigParser()
+
 # Functions
 def main():
-    config = ConfigParser()
     configRead(config)
     setup(config)
     npfMethod()
@@ -34,33 +33,31 @@ def main():
 def configRead(config):
     try:
         config.read(configLocation)
-
         calculation.pixel_pitch = config.get('Pixel Pitch','PIXEL_PITCH')
 
-        if (calculation.pixel_pitch != ""):
+        if (calculation.pixel_pitch == "None"):
             pixelpitch["pixel_width"] = config.get('Pixel Pitch', 'PIXEL_WIDTH')
             pixelpitch["physical_width"] = config.get('Pixel Pitch', 'PHYSICAL_WIDTH')
             
             calculation.pixel_pitch = ( ( float(pixelpitch["physical_width"]) / ( float(pixelpitch["pixel_width"])) ) * 1000 )
 
         else:
-            calculation.pixel_pitch = config.get('Pixel Pitch','PIXEL_PITCH')
+            pass
 
     except:
         raise Exception('Could not open config.cfg, this could be down to a permissions error - \nOr even simply that the file does not exist, please check.\n\nhttps://github.com/AxiomYT/NPF-Rule-Calculator-Python\n\nFor further clarification.\n')
 
 def setup(config):
     calculation.focal_length = (float(config.get('Specific Camera Variables', 'FOCAL_LENGTH')))
-    # calculation.pixel_pitch = ( ( float(pixelpitch["physical_width"]) / ( float(pixelpitch["pixel_width"])) ) * 1000 )
-    calculation.aperture = float(config.get('Specific Camera Variables', 'APERTURE')) 
+    calculation.aperture = float(config.get('Specific Camera Variables', 'APERTURE'))
     
     print('\033[95m' + "\nFocal Length  -", int(calculation.focal_length), " Millimetres" + '\033[0m')
-    print('\033[94m' + "Pixel Pitch   -", round(calculation.pixel_pitch, 2), "Millimetres" + '\033[0m')   # Important that the output is rounded, 
+    print('\033[94m' + "Pixel Pitch   -", round(float(calculation.pixel_pitch), 2), "Millimetres" + '\033[0m')  # Important that the output is rounded, round(calculation.pixel_pitch, 2)
     print('\033[96m' + "Aperture      -", calculation.aperture, "\n" + '\033[0m')
 
 def npfMethod():
     # ( ((35 x aperture) + (30 x pixel pitch)) ÷ focal length ) = Shutter speed in seconds.
-    calculation.shutter_speed = ( (( 35 * calculation.aperture ) + ( 30 * calculation.pixel_pitch )) / (float(calculation.focal_length)))
+    calculation.shutter_speed = ( (( 35 * calculation.aperture ) + ( 30 * float(calculation.pixel_pitch) )) / (float(calculation.focal_length)))
     print('\033[93m' + "Shutter Speed -", (round(calculation.shutter_speed * 2.0) / 2.0), " Seconds\n" + '\033[0m')
 
 # Main
