568ded174e6295f1bf85363243796151f766dc2a
Axiom <40326513+AxiomYT@users.noreply.github.com>
2021-01-09 10:53:59 +0000

    Push 1.5.0 - Made code flow easier
    - More comments
    - More calculation explanation
    - Renamed core Functions to be more reasonable { avoids nasty stuff like - setup(config) & configRead(config) }

--- astrophotography.py
+++ astrophotography.py
@@ -15,42 +15,42 @@ pixelpitch = {
 
 class calculation:
     def __init__(self):
-        self.aperture = ""       # F/Stop No.
+        self.aperture = ""       # Technically we can define aperture as having no unit, as it's a ratio.
         self.pixel_pitch = ""    # Microns
         self.focal_length = ""   # Millimetres
         self.shutter_speed = ""  # Seconds
 
-sys.path
 config = ConfigParser()
 
 # Functions
 def main():
-    configRead(config)
     setup(config)
+    printVariables()
     npfMethod()
     sys.exit()
 
-def configRead(config):
-    try:
-        config.read(configLocation)
-        calculation.pixel_pitch = config.get('Pixel Pitch','PIXEL_PITCH')
+def setup(config):
+    try:                                                                                                                            # Wrapped config.read in a try-except block.  #
+        config.read(configLocation)                                                                                                 # This was done to give the user some -       #
+        calculation.pixel_pitch = config.get('Pixel Pitch','PIXEL_PITCH')                                                           # explanation as to why it failed to execute. #
 
-        if not(calculation.pixel_pitch):
+        calculation.focal_length = (float(config.get('Specific Camera Variables', 'FOCAL_LENGTH')))
+        calculation.aperture = float(config.get('Specific Camera Variables', 'APERTURE'))
+
+        if not(calculation.pixel_pitch):    # If pixel_pitch doesn't have a defined value
             pixelpitch["pixel_width"] = config.get('Pixel Pitch', 'PIXEL_WIDTH')
             pixelpitch["physical_width"] = config.get('Pixel Pitch', 'PHYSICAL_WIDTH')
             
-            calculation.pixel_pitch = ( ( float(pixelpitch["physical_width"]) / ( float(pixelpitch["pixel_width"])) ) * 1000 )
+            # ( ((Physical Width) / (Pixel Width)) x 1000) = Pixel Pitch    # ( Ratio = no unit)
+            calculation.pixel_pitch = ( ((float(pixelpitch["physical_width"])) / ( float(pixelpitch["pixel_width"])) ) * 1000 )
 
-        else:
+        else:   # If pixel_pitch does have a defined value
             pass
 
     except:
         raise Exception('Could not open config.cfg, this could be down to a permissions error - \nOr even simply that the file does not exist, please check.\n\nhttps://github.com/AxiomYT/NPF-Rule-Calculator-Python\n\nFor further clarification.\n')
 
-def setup(config):
-    calculation.focal_length = (float(config.get('Specific Camera Variables', 'FOCAL_LENGTH')))
-    calculation.aperture = float(config.get('Specific Camera Variables', 'APERTURE'))
-    
+def printVariables():
     print('\033[95m' + "\nFocal Length  -", int(calculation.focal_length), " Millimetres" + '\033[0m')
     print('\033[94m' + "Pixel Pitch   -", round(float(calculation.pixel_pitch), 2), "Millimetres" + '\033[0m')  # Important that the output is rounded, round(calculation.pixel_pitch, 2)
     print('\033[96m' + "Aperture      -", calculation.aperture, "\n" + '\033[0m')
